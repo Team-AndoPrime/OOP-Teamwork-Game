7c053d64e19ae9624ee0f26addb39eaa3ecdb1f6
Stoil Kalugerov <skalugerov@gmail.com>
2015-12-10 01:00:00 +0200

    asdasd
    is it working

---
+++ BadSanta/BadSanta/Objects/Items/Gifts/LargeGifts/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BadSanta.Objects.Items.Gifts.LargeGifts
+{
+    public class Car : LargeGift
+    {
+        private const string iconPath = "Images/GiftImages/car";
+        public Car()
+            : base(iconPath)
+        {
+        }
+    }
+}

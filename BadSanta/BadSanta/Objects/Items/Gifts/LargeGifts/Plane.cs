6cf480fdfc289f5b81bc5d3ec6c993d8bc681b56
Stoil Kalugerov <skalugerov@gmail.com>
2015-12-14 14:28:32 +0200

    Gifts added
    added MediumGift Drone and LargeGift Plane

---
+++ BadSanta/BadSanta/Objects/Items/Gifts/LargeGifts/Plane.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BadSanta.Objects.Items.Gifts.LargeGifts
+{
+    public class Plane : LargeGift
+    {
+        private const string iconPath = "Images/GiftImages/Plane";
+
+        public Plane()
+            : base(iconPath)
+        {
+        }
+    }
+}
---
+++ BadSanta/BadSanta/Objects/Items/Gifts/MediumGifts/Drone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BadSanta.Objects.Items.Gifts.LargeGifts
+{
+    public class Drone : MediumGift
+    {
+        private const string iconPath = "Images/GiftImages/Drone";
+
+        public Drone()
+            : base(iconPath)
+        {
+        }
+    }
+}
